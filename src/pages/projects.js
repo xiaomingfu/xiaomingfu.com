31de57699eba9b98b6e4403197f69306adfef8e2
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-28 00:42:58 -0700

    edit projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Projects" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>This is some projects I did</h1>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

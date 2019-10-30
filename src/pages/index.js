7e5170a90ee13e15932ba409733b24294597366d
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-30 00:20:53 -0700

    use navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,14 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import MediaLink from "../components/mediaLink"
+import Navbar from "../components/navbar"
 
 const IndexPage = () => (
   <Layout>
+    <Navbar />
     <SEO title="Xiaoming Fu" />
     <h1>Xiaoming Fu</h1>
     <p>Nice to meet you</p>
-
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>

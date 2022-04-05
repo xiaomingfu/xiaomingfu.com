36262a9782a1e6454bed8b441f303a04addfc06b
Xingyi Wen <xmfswe@gmail.com>
2022-04-04 23:19:49 -0700

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
+import Script from "next/script";
+
+import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="ga"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-2378L7BQGH"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-2378L7BQGH');
+            </script>
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
